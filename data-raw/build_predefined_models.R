73f385df7656ec6fa39e5a09ee1958b4c51eed84
matthiaskloft <mkaystrat88@gmail.com>
2025-07-25 00:26:04 +0200

    update: fixed typo in build_predifined_models.R

--- data-raw/build_predefined_models.R
+++ data-raw/build_predefined_models.R
@@ -1,7 +1,11 @@
 # ANCOVA
 ancova_cont_2arms <- build_model_ancova_cont_2arms()
-ncova_cont_3arms <- build_model_ancova_cont_3arms()
+ancova_cont_3arms <- build_model_ancova_cont_3arms()
 
 
 
-usethis::use_data(ancova_cont_2arms, ancova_cont_3arms, internal = TRUE, overwrite = TRUE)
+# Save the models to R/sysdata.rda
+usethis::use_data(ancova_cont_2arms,
+                  ancova_cont_3arms,
+                  internal = TRUE,
+                  overwrite = TRUE)
